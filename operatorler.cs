96fca3a4d515a7ba77db861783c9e56dc320b023
Neslihan <116304785+Neslihan110@users.noreply.github.com>
2022-12-05 15:41:15 +0300

    Add files via upload

---
+++ operatorler.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {      
+            // Atama işlemi ile atama.
+
+            int x =5;
+            int y =2;
+            y = y+2;
+            Console.WriteLine(y);
+            y +=2;
+            Console.WriteLine(y);
+            y/=1;
+            Console.WriteLine(y);
+            x *=2;
+            Console.WriteLine(x);
+
+            // Mantıksal operatorler  && , || !
+            
+            bool isSucces = true;
+            bool isCompleted = false;
+
+            if(isSucces && isCompleted)
+                Console.WriteLine("Perfesct!");
+
+            if(isSucces || isCompleted)
+                Console.WriteLine("Great!");
+            
+            if (isSucces && isCompleted)
+                Console.WriteLine("Fine!");
+        
+            //İlişkisel operatorler <, >, <=, >=, ==, !=  
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a<b; 
+
+            Console.WriteLine(sonuc);
+            sonuc = a>b;
+            Console.WriteLine(sonuc);
+            sonuc = a>=b;
+            Console.WriteLine(sonuc);
+            sonuc = a<=b;
+            Console.WriteLine(sonuc);
+            sonuc = a==b;
+            Console.WriteLine(sonuc);
+            sonuc = a!=b;
+            Console.WriteLine(sonuc);
+
+            //Aritmetik operatorler /,*,+,-            
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1/sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1+sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1+sayi2;
+            Console.WriteLine(sayi1);
+            sonuc1 = sayi1++;
+
+            // % Mod almak için kullanılan ifade kalanı getiren ifade.
+
+            int sonuc2 = 20%3;
+            Console.WriteLine(sonuc2);
+
+
+            
+        }
+    }
+}
